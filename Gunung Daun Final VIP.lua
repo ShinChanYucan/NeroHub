d5ed9cb3ba81e137132fe2296867b7070fe1f56d
ShinChan Ganteng <94566341+ShinChanYucan@users.noreply.github.com>
2025-08-11 17:36:58 +0700

    Fast summit

--- Gunung Daun Final VIP.lua
+++ Gunung Daun Final VIP.lua
@@ -158,15 +158,15 @@ local function summitLoop()
                 if carriedChar then
                     teleportCharacter(carriedChar, pos + Vector3.new(0, 0, 3))
                 end
-                task.wait(0.9)
+                task.wait(1.2)
             end
         end
-        task.wait(0.9)
+        task.wait(1.2)
         teleportCharacter(player.Character, checkpoints[1])
         if carriedChar then
             teleportCharacter(carriedChar, checkpoints[1] + Vector3.new(0, 0, 3))
         end
-        task.wait(0.9)
+        task.wait(1.2)
     end
 end
 
